a49fed90e40f72ec6e389f4d0d2f7fca74bf6294
Glorfindel <n.tapandzelic1@gmail.com>
2023-01-18 19:00:04 +0100

    added moving background

--- js/Game.js
+++ js/Game.js
@@ -10,12 +10,20 @@ export default class MainGame extends Phaser.Scene {
     this.isGameStarted = false;
 
     this.obstacles;
+    this.background = [];
+    this.backgrounds;
   }
 
   create() {
     this.isGameStarted = false;
-    this.add.image(192, 192, "background").setScale(2);
-    this.player = new Player(this, 75, 40);
+    this.backgrounds = this.physics.add.group();
+    this.background.push(
+      this.backgrounds.create(0, 192, "background").setScale(2)
+    );
+    this.background.push(
+      this.backgrounds.create(1536, 192, "background").setScale(2)
+    );
+    this.player = new Player(this, 75, 150);
 
     this.obstacles = new Obstacles(this.physics.world, this);
     this.obstacles.spawn();
@@ -32,11 +40,18 @@ export default class MainGame extends Phaser.Scene {
         this.obstacles.start();
         this.isGameStarted = true;
         this.sound.play("start");
+        this.backgrounds.setVelocityX(-50);
       }
     });
   }
 
   update() {
     this.obstacles.update();
+
+    for (let i = 0; i < this.background.length; i++) {
+      if (this.background[i].x < -1534) {
+        this.background[i].x = 1536;
+      }
+    }
   }
 }
